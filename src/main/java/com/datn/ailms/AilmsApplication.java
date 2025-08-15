66c3afe5a79431aa1880dd854d8a15e3404800eb
Quang <minhquangru@gmail.com>
2025-08-15 12:56:23 +0700

    first commit

---
+++ src/main/java/com/datn/ailms/AilmsApplication.java
@@ -0,0 +1,13 @@
+package com.datn.ailms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AilmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AilmsApplication.class, args);
+    }
+
+}
