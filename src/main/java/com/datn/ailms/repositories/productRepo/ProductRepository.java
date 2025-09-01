0f91d7dda6ef7f2cdc2167af09f17895e2444f4e
Quang <minhquangru@gmail.com>
2025-09-01 09:52:33 +0700

    Revert "Revert 11 feat/role"

---
+++ src/main/java/com/datn/ailms/repositories/productRepo/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.datn.ailms.repositories.productRepo;
+
+import com.datn.ailms.model.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+    Optional<Product> findBySku(String sku);
+}
---
+++ src/main/java/com/datn/ailms/repositories/warehousetopology/WarehouseRepository.java
@@ -0,0 +1,11 @@
+package com.datn.ailms.repositories.warehousetopology;
+
+import com.datn.ailms.model.entities.Warehouse;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface WarehouseRepository extends JpaRepository<Warehouse, UUID> {
+    Optional<Warehouse> findByCode(String code);
+}
