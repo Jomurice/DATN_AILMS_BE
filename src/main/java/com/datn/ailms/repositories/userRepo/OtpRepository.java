339005c8de1df7754572809eb30d0e4c6a5e9a44
Quang <minhquangru@gmail.com>
2025-09-04 21:05:59 +0700

    fix: verify otp

--- src/main/java/com/datn/ailms/repositories/userRepo/OtpRepository.java
+++ src/main/java/com/datn/ailms/repositories/userRepo/OtpRepository.java
@@ -4,6 +4,10 @@ import com.datn.ailms.model.entities.Otp;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OtpRepository extends JpaRepository<Otp, String> {
+    Optional<Otp> findByUserIdAndOtpCode(String userId, String otpCode);
+
 }
